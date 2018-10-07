52173e7e0ed7652b365c32b49a2c15a68cf6e9fc
刘培阳 <liupeiyang@liupeiyangdeMacBook-Pro.local>
2018-10-07 19:15:40 +0800

    add partitioner

---
+++ hadoop_machine_learning/move_average/NaturalKeyPartitioner.java
@@ -0,0 +1,29 @@
+package hadoop_machine_learning.move_average;
+
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.Partitioner;
+
+public class NaturalKeyPartitioner implements Partitioner<CompositeKey, TimeSeriesData>{
+
+    @Override
+    public int getPartition(CompositeKey key, TimeSeriesData value, int numberOfPartitions) {
+        return Math.abs((int) (hash(key.getName()) % numberOfPartitions));
+    }
+
+    @Override
+    public void configure(JobConf jobConf) {
+
+    }
+
+    /**
+     * adapted from String.hashCode()
+     */
+    static long hash(String str) {
+        long h = 1125899906842597L; // 质数
+        int length = str.length();
+        for (int i = 0; i < length; i++) {
+            h = 31 * h + str.charAt(i);
+        }
+        return h;
+    }
+}
